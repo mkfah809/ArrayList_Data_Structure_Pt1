44a70d54fe26539089f60c2ccf1ce0202bfa7ec5
mkfah809 <minakaramm1@gmail.com>
2021-11-01 11:58:56 -0400

    V1.1

--- src/com/coderscampus/arraylist/Application.java
+++ src/com/coderscampus/arraylist/Application.java
@@ -3,14 +3,14 @@ package com.coderscampus.arraylist;
 public class Application {
 
 	public static void main(String[] args) {
-		CustomArrayList<Integer> elements = new CustomArrayList<>();
+		CustomList<Integer> elements = new CustomArrayList<>();
 				
 		populateNumbers(elements);
 
 	}
 
 	private static void populateNumbers(CustomList<Integer> elements) {
-		for (int element=0; element < 10; element++) {
+		for (int element=0; element < 4; element++) {
 			elements.add(element);
 		}
 	}
--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,19 +1,21 @@
 package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	Object[] items = new Object[10];
+	Object[] items = new Object[3];
+	int arraySize = 0;
+	Object[] newItems = new Object[arraySize * 2];
 
 	@Override
 	public boolean add(T item) {
-		int arraySize = 0;
 		if (arraySize == items.length) {
 			for (int i = 0; i < arraySize; i++) {
-				Object[] newItems = new Object[10];
 				newItems[i] = items[i];
+				items = newItems;
 			}
-			return true;
 		}
-		return false;
+			arraySize++;
+			
+		return true;
 	}
 
 	@Override
