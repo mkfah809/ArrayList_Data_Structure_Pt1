d7543585cd1a9671ff1916b7ae953645f60015be
mkfah809 <minakaramm1@gmail.com>
2021-11-03 14:20:24 -0400

    V1.2

--- src/com/coderscampus/arraylist/Application.java
+++ src/com/coderscampus/arraylist/Application.java
@@ -1,16 +1,30 @@
 package com.coderscampus.arraylist;
 
+import java.util.ArrayList;
+
 public class Application {
 
 	public static void main(String[] args) {
-		CustomList<Integer> elements = new CustomArrayList<>();
-				
+		CustomArrayList<Integer> elements = new CustomArrayList<>();
+	
+		ArrayList<Integer> anElements = new ArrayList<>();		
 		populateNumbers(elements);
+		//elements = anElements;
+		getSizeOfArray(elements);
+		
+	}
 
+	private static void getSizeOfArray(CustomArrayList<Integer> elements) {
+		// then you should validate that all the elements you've inserted
+		//actually exist in your data structure
+		for (int i=0; i<elements.getSize(); i++) {
+		    System.out.println(elements.get(i));
+		}
 	}
 
 	private static void populateNumbers(CustomList<Integer> elements) {
-		for (int element=0; element < 4; element++) {
+		// adding to the element list
+		for (int element=1; element < 60; element++) {
 			elements.add(element);
 		}
 	}
--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,27 +1,33 @@
 package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	Object[] items = new Object[3];
+	Object[] CurrentItems = new Object[3];
 	int arraySize = 0;
-	Object[] newItems = new Object[arraySize * 2];
 
 	@Override
 	public boolean add(T item) {
-		if (arraySize == items.length) {
-			for (int i = 0; i < arraySize; i++) {
-				newItems[i] = items[i];
-				items = newItems;
-			}
+		if (arraySize == CurrentItems.length) {
+			System.out.println("Size of array is " + arraySize + " equal to size of items array is " + CurrentItems.length);			
+			CurrentItems = resizingOfArray();
 		}
 			arraySize++;
 			
 		return true;
 	}
 
+	private Object[] resizingOfArray() {
+		Object[] newItems = new Object[arraySize * 2];
+		for (int i = 0; i < arraySize; i++) {
+			newItems[i] = CurrentItems[i];
+			CurrentItems[i] = newItems[i];
+		}
+		return newItems;
+	}
+
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println(arraySize);
+		return arraySize;
 	}
 
 	@Override
