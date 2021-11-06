a05a8d4a6350bdcb7ea74acbfb5fc3563490e34b
mkfah809 <minakaramm1@gmail.com>
2021-11-06 11:53:49 -0400

    V2.1

--- src/com/coderscampus/arraylist/Application.java
+++ src/com/coderscampus/arraylist/Application.java
@@ -13,13 +13,12 @@ public class Application {
 		getArray.getSizeOfArray(elements);
 	}
 	private void getSizeOfArray(CustomArrayList<Integer> elements) {
-		
-		for (int i=0; i<elements.getSize(); i++) {
+		for (int i=1; i<elements.getSize(); i++) {
 		    System.out.println(i);
 		}
 	}
 	private void populateNumbers(CustomList<Integer> elements) {
-		for (int populateListOfNumbers=0; populateListOfNumbers < 20; populateListOfNumbers++) {
+		for (int populateListOfNumbers=0; populateListOfNumbers <= 20; populateListOfNumbers++) {
 			elements.add(populateListOfNumbers);
 		}
 		System.out.println("********** Array re-sized **********");
